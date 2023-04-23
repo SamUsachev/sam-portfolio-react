565e8cb902a00fd5e54a02483d1b3aa16419cace
SamUsachev <semenusachev10@gmail.com>
2023-04-23 14:58:31 +0300

    styles for about-page (pause-prj)

---
+++ pages/about/Hobbie.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles.scss';
+
+const Hobbie = () => {
+  return (
+    <div className="hobbie">
+      <div className="hobbie_content">
+        <h1>Hobbie</h1>
+        <span></span>
+        <p>
+          Click on a category to view some pictures with some storytelling about
+          my interests...
+        </p>
+        <div className="hobbie_content-main"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Hobbie;
--- pages/projects/Projects.jsx
+++ pages/projects/Projects.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import ProjectCard from './ProjectCard';
 
-import '../styles.scss';
+import './styles.scss';
 
 const Projects = () => {
-  return <div className="projects"></div>;
+  return (
+    <div className="projects">
+      <h1>Projects</h1>
+      <span></span>
+      <div className="projects_card-container">
+        <ProjectCard />
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
